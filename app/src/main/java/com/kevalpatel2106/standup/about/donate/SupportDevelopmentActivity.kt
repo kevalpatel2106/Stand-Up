5f4622c50757d528701445eca85a06dce89eaef3
Keval Patel <kevalpatel2106@gmail.com>
2017-12-18 22:09:18 +0530

    Adding donation link

--- app/src/main/java/com/kevalpatel2106/standup/about/donate/SupportDevelopmentActivity.kt
+++ app/src/main/java/com/kevalpatel2106/standup/about/donate/SupportDevelopmentActivity.kt
@@ -3,8 +3,10 @@ package com.kevalpatel2106.standup.about.donate
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import butterknife.OnClick
 import com.kevalpatel2106.base.uiController.BaseActivity
 import com.kevalpatel2106.standup.R
+import com.kevalpatel2106.standup.SUUtils
 
 /**
  * Created by Kevalpatel2106 on 18-Dec-17.
@@ -31,7 +33,25 @@ class SupportDevelopmentActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support_developement)
         setToolbar(R.id.toolbar, "", true)
+    }
 
-        //TODO Integrate donate button
+    @OnClick(R.id.donate_two_dollar_card)
+    fun donate2Dollar() {
+        SUUtils.openLink(this@SupportDevelopmentActivity, getString(R.string.donate_2_dollar_link))
+    }
+
+    @OnClick(R.id.donate_five_dollar_card)
+    fun donate5Dollar() {
+        SUUtils.openLink(this@SupportDevelopmentActivity, getString(R.string.donate_5_dollar_link))
+    }
+
+    @OnClick(R.id.donate_ten_dollar_card)
+    fun donate10Dollar() {
+        SUUtils.openLink(this@SupportDevelopmentActivity, getString(R.string.donate_10_dollar_link))
+    }
+
+    @OnClick(R.id.donate_twenty_dollar_card)
+    fun donate20Dollar() {
+        SUUtils.openLink(this@SupportDevelopmentActivity, getString(R.string.donate_20_dollar_link))
     }
 }
