358a2f4997947aca2c7914de4c22d0020f0c40cf
Keval Patel <kevalpatel2106@gmail.com>
2017-12-20 22:27:19 +0530

    Fixing the drop box upload

--- scripts/dependency_check.sh
+++ scripts/dependency_check.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 mkdir ../build
 mkdir ../build/reports
-cd ..
 ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json,xml -DoutputDir=../build/reports/
-cd scripts
