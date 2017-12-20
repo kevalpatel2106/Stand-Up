915af508651ec0bf09067fc09844471818d6d124
Keval Patel <kevalpatel2106@gmail.com>
2017-12-20 19:12:14 +0530

    Update upload-artifacts.sh

--- scripts/upload-artifacts.sh
+++ scripts/upload-artifacts.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 #Prepare output folder
-mkdir ../output && cd ../output
-mv ../app/build/outputs/apk ./
-mv ../app/build/outputs/reports ./
-mv ../app/build/reports/logs ./
-mv ../build/reports/profile ./
-rm -rf ../build/reports/profile
-mv ../app/build/reports/ ./
+mkdir /home/travis/build/kevalpatel2106/Stand-Up/output
 
-cd ../scripts
+mv /home/travis/build/kevalpatel2106/Stand-Up/app/build/outputs/apk /home/travis/build/kevalpatel2106/Stand-Up/output/
+
+mv /home/travis/build/kevalpatel2106/Stand-Up/app/build/outputs/reports /home/travis/build/kevalpatel2106/Stand-Up/output/
+
+mv /home/travis/build/kevalpatel2106/Stand-Up/app/build/reports/logs /home/travis/build/kevalpatel2106/Stand-Up/output/
+
+mv /home/travis/build/kevalpatel2106/Stand-Up/build/reports/profile /home/travis/build/kevalpatel2106/Stand-Up/output/
+rm -rf /home/travis/build/kevalpatel2106/Stand-Up/build/reports/profile
+mv /home/travis/build/kevalpatel2106/Stand-Up/build/reports ./
+
+cd /home/travis/build/kevalpatel2106/Stand-Up/scripts
 chmod +x dropbox_uploader.sh
 
 #Upload to artifacts to drop box
-bash dropbox_uploader.sh -p upload /home/travis/build/kevalpatel2106/Stand-Up/outputs /Stand-Up/Artifacts/$TRAVIS_BUILD_NUMBER/output
+bash dropbox_uploader.sh -p upload /home/travis/build/kevalpatel2106/Stand-Up/output /Stand-Up/Artifacts/$TRAVIS_BUILD_NUMBER/output
