e0338f11ffb5b71e7a3d60869a024f2e83ba702a
Keval Patel <kevalpatel2106@gmail.com>
2017-12-20 18:43:48 +0530

    Update dependency_check.sh

--- scripts/dependency_check.sh
+++ scripts/dependency_check.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mkdir ../build
 mkdir ../build/reports
-../gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json,xml -DoutputDir=../build/reports/
+cd ..
+./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json,xml -DoutputDir=../build/reports/
