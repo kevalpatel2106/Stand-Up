0844cf58840ca58d258d2c012ba81e4c645646cb
Keval <kevalpatel2106@gmail.com>
2018-02-19 18:07:40 +0530

    Created separate about module

--- about/src/main/java/com/standup/app/about/SUUtils.kt
+++ about/src/main/java/com/standup/app/about/SUUtils.kt
@@ -13,9 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  *
- */
-
-package com.standup.app.about
+ */package com.standup.app.about
 
 import android.content.Context
 import android.net.Uri
