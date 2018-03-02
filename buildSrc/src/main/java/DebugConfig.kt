2fd1d01a3bd11b7b58f720ce03597b8ac6f87950
Keval Patel <kevalpatel2106@gmail.com>
2018-03-02 12:45:29 +0530

    Adding plugins in using kotlin dsl

---
+++ buildSrc/src/main/java/DebugConfig.kt
@@ -0,0 +1,63 @@
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+/**
+ * Created by Keval on 02/03/18.
+ *
+ * @author [kevalpatel2106](https://github.com/kevalpatel2106)
+ */
+class DebugConfig {
+
+    val APPLICATION_NAME = "Stand Up QA"
+    val PACKAGE_NAME = "com.standup.app.debug"
+
+    //Debug key config
+    val KEYSTORE_FILE = "./misc/debugkey.jks"
+    val KEYSTORE_ALIAS = "stand_up_android"
+    val KEYSTORE_ALIASPASSWORD = "2ea8249e-8ffa-4b77-adde-c70ad4d99edc"
+    val KEYSTORE_PASSWORD = "2ea8249e-8ffa-4b77-adde-c70ad4d99edc"
+
+    // Google keys
+    // See: https://console.developers.google.com/apis/credentials
+    val SERVER_CLIENT_ID = "921769590451-vi8kka7s7tnk5k8u8utiu9i9fsg68hn0.apps.googleusercontent.com"
+    val ANDROID_API_KEY = "AIzaSyBd3eBhrPv3HlTkQjlufwOAEsbwOkllYE4"
+
+    // Facebook credentials (For facebook login)
+    // See: https://developers.facebook.com/docs/facebook-login/android/
+    val FACEBOOK_APP_ID = "154534775066463"
+    val FB_LOGIN_PROTOCOL_SCHEME = "fb154534775066463"
+
+    // Server config
+    val SERVER_BASE_URL = "http://192.168.0.3:8080"
+}
---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+/**
+ * Created by Keval on 02/03/18.
+ *
+ * @author [kevalpatel2106](https://github.com/kevalpatel2106)
+ */
+object Plugins {
+    const val buildTools = "com.android.tools.build:gradle:${Versions.buildPlugin}"
+    const val jacoco = "com.vanniktech:gradle-android-junit-jacoco-plugin:0.11.0"
+    const val dexcount = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.2"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val dokka = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.15"
+    const val versionScan = "com.github.ben-manes:gradle-versions-plugin:0.17.0"
+    const val buildScan = "com.gradle:build-scan-plugin:1.11"
+    const val googleServices = "com.google.gms:google-services:3.2.0"
+    const val firbase = "com.google.firebase:firebase-plugins:1.1.5"
+    const val fabric = "io.fabric.tools:gradle:1.25.1"
+    const val testLogger = "com.adarshr:gradle-test-logger-plugin:1.1.2"
+}
