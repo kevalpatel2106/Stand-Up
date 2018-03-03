acdd2c9003d6e73bf11cc7086dcb03b0b28323df
Keval Patel <kevalpatel2106@gmail.com>
2018-03-03 15:25:55 +0530

    Release v0.2.1

--- buildSrc/src/main/java/AppVersion.kt
+++ buildSrc/src/main/java/AppVersion.kt
@@ -49,6 +49,40 @@
  *
  */
 
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 /**
  * Created by Keval on 02/03/18.
  *
@@ -58,14 +92,14 @@ object AppVersion {
     /**
      * Version code of the application.
      */
-    const val versionCode = 2
+    const val versionCode = 3
 
     // Prepare the version name.
     // Version name scheme: major.minor.patch
 
     private const val versionMajor = 0
     private const val versionMinor = 2
-    private const val versionPatch = 0
+    private const val versionPatch = 1
 
     /**
      * Prepare the version name in [versionMajor].[versionMinor].[versionPatch] format.
