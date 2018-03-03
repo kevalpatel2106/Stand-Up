550a9d1390daf3b58931974e31527f4c598f6d69
Keval Patel <kevalpatel2106@gmail.com>
2018-03-03 11:11:01 +0530

    - Added compiler config. - Added patch number in the version name. - Adding room schema json generation

--- buildSrc/src/main/java/CompilerCofig.kt
+++ buildSrc/src/main/java/CompilerCofig.kt
@@ -32,43 +32,23 @@
  *
  */
 
-/*
- *  Copyright 2018 Keval Patel.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
-
 /**
- * Created by Keval on 02/03/18.
+ * Created by Keval on 03/03/18.
  *
  * @author [kevalpatel2106](https://github.com/kevalpatel2106)
  */
-object AppVersion {
-    /**
-     * Version code of the application.
-     */
-    const val versionCode = 2
+object CompilerCofig {
 
-    // Prepare the version name.
-    // Version name scheme: major.minor.patch
+    const val IS_MULTIDEX_ENABLED = true
 
-    private const val versionMajor = 0
-    private const val versionMinor = 2
-    private const val versionPatch = 0
+    const val ADB_TIMEOUT = 10 * 60 * 1000  // 10 minutes
 
-    /**
-     * Prepare the version name in [versionMajor].[versionMinor].[versionPatch] format.
-     */
-    val versionName = "$versionMajor.$versionMinor.$versionPatch"
+    const val MAX_METHOD_COUNT = 1_10_000
+
+    const val MAX_DEX_PROCESS_COUNT = 6
+    const val PREDEX_LIBRARIES = true
+
+    const val CRUNCHER_PROCESS_COUNT = 4
+
+    const val LINT_ABORT_ON_ERROR = false
 }
--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -32,15 +32,58 @@
  *
  */
 
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 /**
  * Created by Keval on 02/03/18.
+ * Default configuration for the application.
  *
  * @author [kevalpatel2106](https://github.com/kevalpatel2106)
  */
 object DefaultConfig {
+
+    /**
+     * Minimum supported SDK.
+     */
     const val min = 21
+
+    /**
+     * Target SDK.
+     */
     const val target = 26
+
+    /**
+     * Compile SDK.
+     */
     const val compile = 26
+
+    /**
+     * Build tool version.
+     */
     const val buildToolVersion = "27.0.2"
+
+    /**
+     * Base package name of the application. The package name used in AndroidManifest.xml may vary
+     * based on build types.
+     */
     const val applicationId = "com.standup.app"
+
+    const val testRunnerClass = "com.kevalpatel2106.testutils.CustomTestRunner"
+
+    const val supportedResConfig = "en"
 }
