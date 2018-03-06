0d5bf0f588a1f898c2d116fe1bafb8758de5d73e
Keval Patel <kevalpatel2106@gmail.com>
2018-03-06 22:30:18 +0530

    Released 0.3.1.

--- buildSrc/src/main/java/AppVersion.kt
+++ buildSrc/src/main/java/AppVersion.kt
@@ -83,6 +83,23 @@
  *
  */
 
+/*
+ *  Copyright 2018 Keval Patel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 /**
  * Created by Keval on 02/03/18.
  *
@@ -92,14 +109,14 @@ object AppVersion {
     /**
      * Version code of the application.
      */
-    const val versionCode = 4
+    const val versionCode = 5
 
     // Prepare the version name.
     // Version name scheme: major.minor.patch
 
     private const val versionMajor = 0
     private const val versionMinor = 3
-    private const val versionPatch = 0
+    private const val versionPatch = 1
 
     /**
      * Prepare the version name in [versionMajor].[versionMinor].[versionPatch] format.
