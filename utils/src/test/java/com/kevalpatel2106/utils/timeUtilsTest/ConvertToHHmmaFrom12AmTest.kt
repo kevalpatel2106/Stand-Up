7c536afff43e39194429a452a6386c1c0bada102
Keval Patel <kevalpatel2106@gmail.com>
2018-03-07 21:43:51 +0530

    Fixing failing tests

--- utils/src/test/java/com/kevalpatel2106/utils/timeUtilsTest/ConvertToHHmmaFrom12AmTest.kt
+++ utils/src/test/java/com/kevalpatel2106/utils/timeUtilsTest/ConvertToHHmmaFrom12AmTest.kt
@@ -61,7 +61,7 @@ class ConvertToHHmmaFrom12AmTest {
         @Throws(IOException::class)
         fun testConvertToHHmmaFrom12Am() {
             Assert.assertEquals(
-                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(value)),
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(value)).replace("12", "00"),
                     TimeUtils.convertToHHmmaFrom12Am(value)
             )
         }
